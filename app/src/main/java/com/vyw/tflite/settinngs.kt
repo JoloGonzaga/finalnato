f430d746dc473d879f225ef6c4644c77ad3a27e6
jologonzaga <gonzagalloyd06@gmail.com>
2023-01-08 17:53:46 +0800

    done update1

--- app/src/main/java/com/vyw/tflite/settinngs.kt
+++ app/src/main/java/com/vyw/tflite/settinngs.kt
@@ -1,6 +1,7 @@
 package com.vyw.tflite
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,7 +11,18 @@ class settinngs : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settinngs)
+        val PICK_mp3_FILE = 2
+
+        fun openfile_btnClick(pickerInitialUri: Uri) {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "application/mp3"
 
 
+
+
+
+            }
         }
     }
+}
